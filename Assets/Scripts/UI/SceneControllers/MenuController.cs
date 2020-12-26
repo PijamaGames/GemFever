a6574cfbbd4458aa9af91a2021d1d7be886d0232
Pedro Casas Martínez <pedrocasas876@gmail.com>
2020-12-26 12:24:53 +0100

    interfaz de menú provisional

---
+++ Assets/Scripts/UI/SceneControllers/MenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] Bilingual welcomeText;
+    [SerializeField] Button exitButton;
+
+    private void Start()
+    {
+        if(Client.user != null)
+        {
+            welcomeText.spanishText = "¡Bienvenido " + Client.user.id + "!";
+            welcomeText.englishText = "Welcome " + Client.user.id + "!";
+            welcomeText.UpdateLanguage();
+        }
+        //ClientSignedIn.signedOutEvent += ()=>
+        exitButton.onClick.AddListener(() => ClientSignedIn.TrySignOut());
+    }
+}
