40bd027790d54e54094c90bb5d8fe5c62622db1f
Pedro Casas Martínez <pedrocasas876@gmail.com>
2020-12-29 12:19:40 +0100

    interfaz de escena de juego, camera size controller

---
+++ Assets/Scripts/UI/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] float defaultSize = 3.91f;
+    [SerializeField] float aspect = 2f;
+    private Camera cam;
+    Vector3 cameraPos;
+    float defaultWidth;
+    float lastAspect;
+
+   
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        cam.orthographicSize = defaultSize;
+        defaultWidth = defaultSize * aspect;
+        lastAspect = 0f;
+    }
+
+    private void Update()
+    {
+        if(cam.aspect != lastAspect)
+        {
+            lastAspect = cam.aspect;
+            cam.orthographicSize = defaultWidth / cam.aspect;
+        }
+    }
+}
