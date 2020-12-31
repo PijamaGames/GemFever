41bb0449e87a8199ef599a7fa3bce40dac968295
alejandracc <alejandracasado97@gmail.com>
2020-12-31 19:53:35 +0100

    colores por codigo xd

---
+++ Assets/Scripts/UI/SceneControllers/CustomizeAvatarController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomizeAvatarController : MonoBehaviour
+{
+
+    [SerializeField] GameObject skinParent;
+    [SerializeField] GameObject colorParent;
+    private Image[] skins;
+    private Image[] colors;
+
+    public static Color[] skinColors={ new Color(1f, 0.8789797f, 0.5707547f), new Color(255/255f,216/255f,177/255f), new Color(170/255f,127/255f,82/255f),new Color(125/255f,83/255f,42/255f), new Color(75/255f,44/255f,13/255f)};
+    public static Color[] favColors={Color.black, Color.blue, Color.cyan, Color.gray, Color.green, Color.magenta};
+
+
+    private void Start()
+    {
+        skins = skinParent.GetComponentsInChildren<Image>();
+        colors = colorParent.GetComponentsInChildren<Image>();
+
+        for (int i=0; i<skins.Length; i++)
+        {
+            skins[i].color = skinColors[i];
+        }
+        for (int i = 0; i < colors.Length; i++)
+        {
+            colors[i].color = favColors[i];
+        }
+
+    }
+
+}
