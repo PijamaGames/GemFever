c4f37c89e4c0d432e9ab8f1757a6f91e98ddb251
Pedro Casas Martínez <pedrocasas876@gmail.com>
2020-12-31 18:54:56 +0100

    player avatar controller

---
+++ Assets/Scripts/Player/PlayerAvatar.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAvatar : MonoBehaviour
+{
+
+    [HideInInspector] Material skinMat;
+    [SerializeField] Renderer skinRenderer;
+
+    [HideInInspector] Material shirtMat;
+    [SerializeField] Renderer shirtRenderer;
+
+    [HideInInspector] Material pantsMat;
+    [SerializeField] Renderer pantsRenderer;
+
+    [HideInInspector] Material hatMat;
+    [SerializeField] Renderer hatRenderer;
+
+    [HideInInspector] Material faceMat;
+    [SerializeField] Renderer faceRenderer;
+
+    private void Start()
+    {
+        skinMat = Instantiate(skinRenderer.sharedMaterial);
+        skinRenderer.sharedMaterial = skinMat;
+
+        shirtMat = Instantiate(shirtRenderer.sharedMaterial);
+        shirtRenderer.sharedMaterial = shirtMat;
+
+        pantsMat = Instantiate(pantsRenderer.sharedMaterial);
+        pantsRenderer.sharedMaterial = pantsMat;
+
+        hatMat = Instantiate(hatRenderer.sharedMaterial);
+        hatRenderer.sharedMaterial = hatMat;
+
+        faceMat = Instantiate(faceRenderer.sharedMaterial);
+        faceRenderer.sharedMaterial = faceMat;
+    }
+
+    public void UpdateVisuals(User user)
+    {
+        
+    }
+
+}
