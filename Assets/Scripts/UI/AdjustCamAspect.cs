3fbcada78bcf7f214aa0babc4ca7ae1297d34242
Pedro Casas Martínez <pedrocasas876@gmail.com>
2021-01-04 13:00:06 +0100

    fix background limits

---
+++ Assets/Scripts/UI/AdjustCamAspect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustCamAspect : MonoBehaviour
+{
+    Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("aspect: " + cam.aspect + " fov: " + cam.fieldOfView);
+    }
+}
