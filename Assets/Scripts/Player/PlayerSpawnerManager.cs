32004f60b82a202d098c2e4262aae08811b06808
Martín Ariza García <martinarizagarcia@gmail.com>
2021-01-04 19:09:01 +0100

    Hecha a un jugador si pasa mucho tiempo sin hacer un input inicial

--- Assets/Scripts/Player/PlayerSpawnerManager.cs
+++ Assets/Scripts/Player/PlayerSpawnerManager.cs
@@ -7,7 +7,12 @@ public class PlayerSpawnerManager : MonoBehaviour
 {
     public static bool isInHub = false;
 
+    private bool hasJoined = false;
+    private bool anyInputDone = false;
+    private bool kicking = false;
+
     [SerializeField] bool _isInHub;
+    [SerializeField] float timeToKickOut = 10f;
 
     [SerializeField] List<GameObject> playerSpawnLocations = new List<GameObject>();
     [SerializeField] int maxPlayers = 4;
@@ -16,13 +21,51 @@ public class PlayerSpawnerManager : MonoBehaviour
     private void Start()
     {
         isInHub = _isInHub;
+
+        if(!GameManager.isLocalGame || true)
+            StartCoroutine(KickCountdown());
+    }
+
+    IEnumerator KickCountdown()
+    {
+        yield return new WaitForSecondsRealtime(timeToKickOut);
+        if (!anyInputDone)
+        {
+            kicking = true;
+            ClientInRoom.Exit();
+        }
     }
 
     void OnPlayerJoined(PlayerInput playerInput)
     {
         if (currentPlayers >= maxPlayers) return;
 
-        SpawnPlayerAtLocation(playerInput.gameObject);
+        if(!GameManager.isLocalGame)
+        {
+            //Online Game
+            if (kicking)
+            {
+                Destroy(playerInput.gameObject);
+                return;
+            }
+
+            if (!hasJoined)
+            {
+                hasJoined = true;
+                anyInputDone = true;
+                //TODO Avisar al server de que se quiere spawnear el jugador y hacer que lo cree el playerJoiner
+                SpawnPlayerAtLocation(playerInput.gameObject);
+            }
+            else
+                Destroy(playerInput.gameObject);
+        }
+        //Local Game
+        else
+        {
+            anyInputDone = true;
+            SpawnPlayerAtLocation(playerInput.gameObject);
+        }
+            
     }
 
     void SpawnPlayerAtLocation(GameObject player)
