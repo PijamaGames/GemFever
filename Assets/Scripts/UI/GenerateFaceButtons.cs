56793db24d3420d9f4181c9965ed467f85545300
alejandracc <alejandracasado97@gmail.com>
2021-01-09 09:36:14 +0100

    main

---
+++ Assets/Scripts/UI/GenerateFaceButtons.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateFaceButtons : MonoBehaviour
+{
+    [SerializeField] GameObject buttonPrefab;
+    [SerializeField] Transform facesContainer;
+    FaceButton[] faceButtons;
+    private int maxFaces;
+    void Start()
+    {
+        maxFaces = Client.user.avatar_face.Length;
+        faceButtons = new FaceButton[maxFaces];
+        for (int i = 0; i < maxFaces; i++)
+        {
+            faceButtons[i] = Instantiate(buttonPrefab, facesContainer).GetComponent<FaceButton>();
+            faceButtons[i].UpdateVisuals(FaceTextures.facesTextures[FaceTextures.facesForRandom[i]]);
+        }
+    }
+
+}
