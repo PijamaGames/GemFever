3168f8db362db247b6d7932bb5f8a10199bf54a2
pedro876 <pedrocasas876@gmail.com>
2021-01-11 19:52:54 +0100

    webgl input fields

---
+++ Assets/Scripts/KeyboardWebGL/ActivateKeyboard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateKeyboard : MonoBehaviour
+{
+    [SerializeField] WebGLKeyboard.KeyboardController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller.enabled = GameManager.isHandheld;
+    }
+}
