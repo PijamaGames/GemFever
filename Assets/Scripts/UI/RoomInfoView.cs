93b5fad523ec442d71a9588af9910729d0322cff
alejandracc <alejandracasado97@gmail.com>
2021-01-11 19:47:21 +0100

    desde local se eligen niveles

--- Assets/Scripts/UI/RoomInfoView.cs
+++ Assets/Scripts/UI/RoomInfoView.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class RoomInfoView : MonoBehaviour
 {
@@ -10,6 +11,12 @@ public class RoomInfoView : MonoBehaviour
     public Bilingual numPlayers;
     private Button btn;
     string player;
+
+    float moveYOnPressed = -4.5f;
+
+    private Vector3 originalPosName;
+    private Vector3 originalPosNumPlayers;
+    private TextMeshProUGUI[] text;
 
     private void Start()
     {
@@ -19,6 +26,23 @@ public class RoomInfoView : MonoBehaviour
             Debug.Log("PRESS JOIN: " + player);
             ClientSignedIn.JoinRoom(player);
         });
+
+        originalPosName = new Vector3();
+        originalPosName = playerName.rectTransform.localPosition;
+
+        originalPosNumPlayers = new Vector3();
+        originalPosNumPlayers = numPlayers.GetComponent<TextMeshProUGUI>().rectTransform.localPosition;
+
+        EventTrigger trigger = btn.gameObject.AddComponent<EventTrigger>();
+        var pointerDown = new EventTrigger.Entry();
+        pointerDown.eventID = EventTriggerType.PointerDown;
+        pointerDown.callback.AddListener((e) => Pressed());
+        trigger.triggers.Add(pointerDown);
+
+        var pointerUp = new EventTrigger.Entry();
+        pointerUp.eventID = EventTriggerType.PointerUp;
+        pointerUp.callback.AddListener((e) => Released());
+        trigger.triggers.Add(pointerUp);
     }
 
     public void UpdateVisuals(PlayerRoomInfo info)
@@ -34,5 +58,24 @@ public class RoomInfoView : MonoBehaviour
         gameObject.SetActive(info != null);
     }
 
+    public void Pressed()
+    {
+        Vector3 posName =originalPosName;
+        Vector3 posNumPlayers =originalPosNumPlayers;
+
+        posName.y += moveYOnPressed;
+        posNumPlayers.y += moveYOnPressed;
+
+        playerName.rectTransform.localPosition = posName;
+        numPlayers.GetComponent<TextMeshProUGUI>().rectTransform.localPosition = posNumPlayers;
+
+    }
+
+    public void Released()
+    {
+        playerName.rectTransform.localPosition = originalPosName;
+        numPlayers.GetComponent<TextMeshProUGUI>().rectTransform.localPosition = originalPosNumPlayers;
+        
+    }
 
 }
